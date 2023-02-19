4a67688946a340c26cd564c203b13274ed8bd167
Hpsaturn <hpsaturn@gmail.com>
2023-02-19 17:01:51 +0100

    refactor sound methods and added two demos, blink and radio via CLI

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,10 @@
 // Required Libraries (Download zips and add to the Arduino IDE library).
 #include "Arduino.h"
-#include <ESP32WifiCLI.hpp>
-#include <WM8978.h> // https://github.com/CelliesProjects/wm8978-esp32
-#include <Audio.h>  // https://github.com/schreibfaul1/ESP32-audioI2S
+#include <ESP32WifiCLI.hpp> 
 #include <gui.hpp>
-
-// T-Audio 1.6 WM8978 I2C pins.
-#define I2C_SDA     19
-#define I2C_SCL     18
-
-// T-Audio 1.6 WM8978 I2S pins.
-#define I2S_BCK     33
-#define I2S_WS      25
-#define I2S_DOUT    26
-
-// T-Audio 1.6 WM8978 MCLK gpio number
-#define I2S_MCLKPIN  0
-
-#define WIFI_RETRY_CONNECTION 4
+#include <sound.hpp>
 
 #define TAG "WM8978"
-
-Audio audio;
-WM8978 dac;
 
 
 bool setup_mode = false;
@@ -87,6 +69,17 @@ void blink(String opts) {
   guiDemo();
 }
 
+void radio(String opts) {
+  // WiFi Settings here.
+  ESP_LOGI(TAG, "Connected. Starting MP3...");
+  // Enter your Icecast station URL here.
+  audio.setVolume(21);
+  audio.connecttohost("http://hestia2.cdnstream.com/1458_128");
+  // Volume control.
+  dac.setSPKvol(30); // Change volume here for board speaker output (Max 63).
+  dac.setHPvol(50, 50); // Change volume here for headphone jack left, right channel.
+}  
+
 void reboot(String opts){
   ESP.restart();
 }
@@ -107,16 +100,18 @@ void setup() {
   Serial.flush();       // Only for showing the message on serial 
   delay(100);
   guiInit();            // Initialize LED stripe to off
+  initAudio();          // Audio drivers only
+
   wcli.setCallback(new mESP32WifiCLICallbacks());
-  wcli.disableConnectInBoot();
+  // wcli.disableConnectInBoot();
   wcli.setSilentMode(true);
   wcli.begin();         // Alternatively, you can init with begin(115200) 
 
  // custom commands:
   wcli.term->add("sleep", &sleep,     "\t<mode> <time> ESP32 will enter to sleep mode");
-  wcli.term->add("blink", &blink,     "\t<times> <millis> LED blink x times each x millis");
   wcli.term->add("reboot", &reboot,   "\tperform a ESP32 reboot");
-  // setup mode commands:
+  wcli.term->add("blink", &blink,     "\t<brightness> Adafruit LEDs demo");
+  wcli.term->add("radio", &radio,     "\tstarting radio broadcast demo");
   wcli.term->add("exit", &wcli_exit,  "\texit of the setup mode. AUTO EXIT in 10 seg! :)");
   wcli.term->add("setup", &wcli_setup,"\tTYPE THIS WORD to start to configure the device :D\n");
 
@@ -131,61 +126,9 @@ void setup() {
   else
     Serial.println("==>[INFO] Time for initial setup over. Booting..\r\n");
 
-
-  // Setup wm8978 I2C interface.
-  if (!dac.begin(I2C_SDA, I2C_SCL)) {
-    ESP_LOGE(TAG, "Error setting up dac: System halted.");
-    while (1) delay(100);
-  }
-
-  // Select I2S pins
-  audio.setPinout(I2S_BCK, I2S_WS, I2S_DOUT);
-  audio.i2s_mclk_pin_select(I2S_MCLKPIN);
-
-  // WiFi Settings here.
-  ESP_LOGI(TAG, "Connected. Starting MP3...");
-  // Enter your Icecast station URL here.
-  audio.setVolume(21);
-  audio.connecttohost("http://hestia2.cdnstream.com/1458_128");
-  // Volume control.
-  dac.setSPKvol(30); // Change volume here for board speaker output (Max 63).
-  dac.setHPvol(50, 50); // Change volume here for headphone jack left, right channel.
-  
 }
 
 void loop() {
   wcli.loop();
   audio.loop();
-}
-
-// optional
-void audio_info(const char *info){
-    Serial.print("info        "); Serial.println(info);
-}
-void audio_id3data(const char *info){  //id3 metadata
-    Serial.print("id3data     ");Serial.println(info);
-}
-void audio_eof_mp3(const char *info){  //end of file
-    Serial.print("eof_mp3     ");Serial.println(info);
-}
-void audio_showstation(const char *info){
-    Serial.print("station     ");Serial.println(info);
-}
-void audio_showstreamtitle(const char *info){
-    Serial.print("streamtitle ");Serial.println(info);
-}
-void audio_bitrate(const char *info){
-    Serial.print("bitrate     ");Serial.println(info);
-}
-void audio_commercial(const char *info){  //duration in sec
-    Serial.print("commercial  ");Serial.println(info);
-}
-void audio_icyurl(const char *info){  //homepage
-    Serial.print("icyurl      ");Serial.println(info);
-}
-void audio_lasthost(const char *info){  //stream URL played
-    Serial.print("lasthost    ");Serial.println(info);
-}
-void audio_eof_speech(const char *info){
-    Serial.print("eof_speech  ");Serial.println(info);
 }
---
+++ src/sound.hpp
@@ -0,0 +1,62 @@
+#include <WM8978.h> // https://github.com/CelliesProjects/wm8978-esp32
+#include <Audio.h>  // https://github.com/schreibfaul1/ESP32-audioI2S
+
+// T-Audio 1.6 WM8978 I2C pins.
+#define I2C_SDA     19
+#define I2C_SCL     18
+
+// T-Audio 1.6 WM8978 I2S pins.
+#define I2S_BCK     33
+#define I2S_WS      25
+#define I2S_DOUT    26
+
+// T-Audio 1.6 WM8978 MCLK gpio number
+#define I2S_MCLKPIN  0
+
+Audio audio;
+WM8978 dac;
+
+
+// optional
+void audio_info(const char *info){
+    Serial.print("info        "); Serial.println(info);
+}
+void audio_id3data(const char *info){  //id3 metadata
+    Serial.print("id3data     ");Serial.println(info);
+}
+void audio_eof_mp3(const char *info){  //end of file
+    Serial.print("eof_mp3     ");Serial.println(info);
+}
+void audio_showstation(const char *info){
+    Serial.print("station     ");Serial.println(info);
+}
+void audio_showstreamtitle(const char *info){
+    Serial.print("streamtitle ");Serial.println(info);
+}
+void audio_bitrate(const char *info){
+    Serial.print("bitrate     ");Serial.println(info);
+}
+void audio_commercial(const char *info){  //duration in sec
+    Serial.print("commercial  ");Serial.println(info);
+}
+void audio_icyurl(const char *info){  //homepage
+    Serial.print("icyurl      ");Serial.println(info);
+}
+void audio_lasthost(const char *info){  //stream URL played
+    Serial.print("lasthost    ");Serial.println(info);
+}
+void audio_eof_speech(const char *info){
+    Serial.print("eof_speech  ");Serial.println(info);
+}
+
+void initAudio() {
+  // Setup wm8978 I2C interface.
+  if (!dac.begin(I2C_SDA, I2C_SCL)) {
+    ESP_LOGE(TAG, "Error setting up dac: System halted.");
+    while (1) delay(100);
+  }
+
+  // Select I2S pins
+  audio.setPinout(I2S_BCK, I2S_WS, I2S_DOUT);
+  audio.i2s_mclk_pin_select(I2S_MCLKPIN);
+}
